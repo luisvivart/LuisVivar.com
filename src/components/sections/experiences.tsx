f06ba4377c11dacf750fd4de6ea501e57a727791
Luisvivart <info@Luisvivar.com>
2025-08-20 14:09:41 -0400

    added experience to the nav menu

--- src/components/sections/experiences.tsx
+++ src/components/sections/experiences.tsx
@@ -6,13 +6,13 @@ import Container from '@/components/layout/container';
 
 const ExperienceSection = () => {
   return (
-    <Container className="bg-gray-50">
+    <Container id="experience" className="bg-gray-50">
       <div className="flex flex-col items-center gap-4">
         <div className="self-center">
           <Tag label="Experience" />
         </div>
         <Typography variant="subtitle" className="max-w-xl text-center">
-          Here is a quick summary of my most recent, relevant experiences:
+          Here is a quick summary of my most recent relevant experience:
         </Typography>
       </div>
 
